f27d408ecdd0743224941d325cc6d0119d03d6e6
Yahya-ADLOUNI <adlouniyahya14@gmail.com>
2022-10-05 00:44:48 +0200

    Added all the migrations

---
+++ database/migrations/2022_10_04_223152_create_artworks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artworks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('foreign_id');
+            $table->unsignedBigInteger('source_id');
+            $table->foreign('source_id')->on('sources')->references('id')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artworks');
+    }
+};
---
+++ database/migrations/2022_10_04_224050_create_interests_artworks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('interests_artworks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('interest_id');
+            $table->unsignedBigInteger('artwork_id');
+            $table->foreign('interest_id')->on('interests')->references('id')->onDelete('cascade');
+            $table->foreign('artwork_id')->on('artworks')->references('id')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('interests_artworks');
+    }
+};
